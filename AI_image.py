fdc84bbca2206892c4cd88c2f7fac34d5ccb84de
Ahsan Ullah Jameel <112726701+Ahsaan100@users.noreply.github.com>
2024-06-11 05:10:04 +0500

    Update AI_image.py

--- AI_image.py
+++ AI_image.py
@@ -12,7 +12,7 @@ r = requests.post('https://clipdrop-api.co/text-to-image/v1',
   files = {
       'prompt': (None, str(text_input_val), 'text/plain')
   },
-  headers = { 'x-api-key': "5b595a43bed99e94a779fe681176ca93631c0bb3ae4a4a689c148f8762adc29bc9b0aa7a8dc60f93382998550747682f"}
+  headers = { 'x-api-key': KEY}
 )
 if (r.ok):
     image = Image.open(io.BytesIO(r.content))
